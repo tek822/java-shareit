fb1251fe247a2cd84997f4293072a5b5944422f6
Gusev Roman <tek822@yandex.ru>
2023-05-22 00:32:56 +0300

    fix: CheckStyle error: MethodLineSeparator

--- src/main/java/ru/practicum/shareit/booking/service/BookingService.java
+++ src/main/java/ru/practicum/shareit/booking/service/BookingService.java
@@ -6,9 +6,14 @@ import ru.practicum.shareit.booking.dto.BookingDto;
 import java.util.List;
 
 public interface BookingService {
+
     BookingDto add(long userId, BookingSimpleDto bookingDto);
+
     BookingDto approve(long bookingId, long userId, boolean approve);
+
     BookingDto get(long bookingId, long userId);
+
     List<BookingDto> getOwnBookings(long userId, String state);
+
     List<BookingDto> getBookingsForOwnItems(long userId, String state);
 }
