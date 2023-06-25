8f1d5b98919eb8e615e0d269e7c8b7223638728b
Gusev Roman <tek822@yandex.ru>
2023-06-25 05:04:45 +0300

    fix: unused import deleted

--- src/main/java/ru/practicum/shareit/user/controller/UserErrorHandler.java
+++ src/main/java/ru/practicum/shareit/user/controller/UserErrorHandler.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.user.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.modelmapper.ValidationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
