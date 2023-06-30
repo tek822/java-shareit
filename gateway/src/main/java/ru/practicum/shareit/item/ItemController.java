146cecf43835c477be6c7d87fe19be917e1d377c
Gusev Roman <tek822@yandex.ru>
2023-06-30 18:56:22 +0300

    fix: checkstyle fixes

--- gateway/src/main/java/ru/practicum/shareit/item/ItemController.java
+++ gateway/src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.item;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.PositiveOrZero;
-import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
